56e637d6e1122cf08470915b0e34a3bff40645b2
ElRuflas <128366266+ElRuflas@users.noreply.github.com>
2024-04-16 13:07:35 +0200

    Crear Usuario
    TG-3 #donec
    Funcionalidad de añadir usuario al sistema realizada.

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,5 +1,28 @@
 package Sprint1;
 
 public class Usuario {
-
+	private Integer id;
+	private String nombre;
+	
+	public Usuario() {
+		
+	}
+	public Usuario(Integer id,String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
