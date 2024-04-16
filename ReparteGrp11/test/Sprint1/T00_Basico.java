fa5c3fb3f73673f917869ecff0fbe01c7db96039
Lucas <lucasdcgrei@gmail.com>
2024-04-16 12:14:49 +0200

    Inicial con primer caso de pruebas

---
+++ ReparteGrp11/test/Sprint1/T00_Basico.java
@@ -0,0 +1,14 @@
+package Sprint1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class T00_Basico {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
