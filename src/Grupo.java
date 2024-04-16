d239afbdbf8542ba1dd8e5112d7cd34f51dfcf42
Brais Lence Martínez <brais.lence@rai.usc.es>
2024-04-16 13:49:10 +0200

    TG-4 #donec
    Implementadas las funcionalidades de añadir un usuario o múltiples usuarios al grupo.

--- src/Grupo.java
+++ src/Grupo.java
@@ -1,5 +1,6 @@
 package Sprint1;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -41,5 +42,27 @@ public class Grupo {
     
 	public void anadirGasto(Gasto gasto) {
 	
+	}
+
+	public void anhadirUsuario(Usuario u) {
+		if(u != null && !this.users.contains(u)) {
+			this.users.add(u);
+			System.out.println("Usuario " + u + " anhadido correctamente al grupo.");
+		}
+		else {
+			System.out.println("Error: el usuario " + u + " no existe o ya esta incluido en el grupo.");
+		}
+	}
+	
+	public void anhadirMultiplesUsuarios(ArrayList<Usuario> usuarios) {
+		if(usuarios != null && !usuarios.isEmpty()) {
+			for(Usuario u : usuarios) {
+				anhadirUsuario(u);
+			}
+			System.out.println("Los usuarios han sido anhadidos correctamente al grupo.");
+		}
+		else {
+			System.out.println("Error: la lista no existe o está vacía.");
+		}
 	}
 }
