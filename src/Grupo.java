9965482ce79e99c5e1df8d35d002161f04a6cd10
Lucas <lucasdcgrei@gmail.com>
2024-04-16 13:28:14 +0200

    crearGrupo

--- src/Grupo.java
+++ src/Grupo.java
@@ -1,5 +1,42 @@
 package Sprint1;
 
+import java.util.HashSet;
+import java.util.Random;
+
 public class Grupo {
 
+    Integer id;
+    String nombre;
+    HashSet<Usuario> users;
+
+    public Grupo(String nombre, HashSet<Usuario> users) {
+        id = new Random().nextInt();
+        nombre = this.nombre;
+        setUsers(users);
+
+    }
+
+    public HashSet<Usuario> getUsers() {
+        return users;
+    }
+
+    public void setUsers(HashSet<Usuario> users) {
+        if (users != null && !users.isEmpty()) {
+            this.users.addAll(users);
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
 }
