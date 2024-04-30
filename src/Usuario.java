8300956217852130d2a14baecb1c1e69d13fa287
Lucas <lucasdcgrei@gmail.com>
2024-04-30 14:18:08 +0200

    TG-3 #donecb

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,32 +2,51 @@ package Sprint1;
 
 public class Usuario {
 
-	private Integer id;
-	private String nombre;
-	
-	public Usuario() {
-		
-	}
-	public Usuario(Integer id,String nombre) {
-		this.id = id;
-		this.nombre = nombre;
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
+    private Integer id;
+    private String nombre;
 
-	public void crearGasto(Double cantidad, Grupo grupo){
-		Gasto g = new Gasto(cantidad, grupo, this);
-	}
+    public Usuario() {
+
+    }
+
+    public Usuario(Integer id, String nombre) {
+        if (id != null) {
+            this.id = id;
+            if (nombre != null) {
+                if (nombre.matches("^[a-zA-Z0-9]+$")) {
+                    this.nombre = nombre;
+                } else {
+                    //lanzar excepcion
+                    //System.out.println("nombre no puede contener otra cosa que numeros y letras");
+                }
+
+            }else {
+                    //lanzar excepcion
+                    //System.out.println("nombre no puede ser nulo");
+                }
+        } else {
+            //lanzar excepcion
+            //System.out.println("ID no puede ser nulo");
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void crearGasto(Double cantidad, Grupo grupo) {
+        Gasto g = new Gasto(cantidad, grupo, this);
+    }
 }
--- test/T03_CrearUsuario.java
+++ test/T03_CrearUsuario.java
@@ -6,51 +6,43 @@ import org.junit.jupiter.api.Test;
 
 class T03_CrearUsuario {
 
-	@Test
-	 void CP1() {
-	        // Caso 1: ID positivo, nombre válido.
-	        int id1 = 123;
-	        String nombre1 = "Juan";
-	        Usuario usuario1 = new Usuario(id1, nombre1);
-	        assertEquals(nombre1, usuario1.getNombre());
-	        assertEquals(id1,usuario1.getId());
-	    }
+    //Caja negra
+    @Test
+    void CP1() {
+        // Caso 1: ID positivo, nombre válido.
+        int id1 = 123;
+        String nombre1 = "Juan";
+        Usuario usuario1 = new Usuario(id1, nombre1);
+        assertEquals(nombre1, usuario1.getNombre());
+        assertEquals(id1, usuario1.getId());
+    }
 
-	    @Test
-	    void CP2() {
-	        // Caso 2: ID negativo, nombre válido.
-	        int id2 = -456;
-	        String nombre2 = "juan";
-	        Usuario usuario2 = new Usuario(id2, nombre2);
-	        assertTrue(usuario2.getId() < 0);
-	        assertEquals("juan",usuario2.getNombre());
-	    }
+    @Test
+    void CP2() {
+        // Caso 2: ID negativo, nombre válido.
+        int id2 = -456;
+        String nombre2 = "juan";
+        Usuario usuario2 = new Usuario(id2, nombre2);
+        assertTrue(usuario2.getId() < 0);
+        assertEquals("juan", usuario2.getNombre());
+    }
 
-	    @Test
-	    void CP3() {
-	        // Caso 3: ID cero, nombre válido.
-	        int id3 = 0;
-	        String nombre3 = "pepe";
-	        Usuario usuario3 = new Usuario(id3, nombre3);
-	        assertEquals(usuario3.getId(),0);
-	    }
+    @Test
+    void CP3() {
+        // Caso 4: ID válido, nombre nulo.
+        int id4 = 789;
+        String nombre4 = null;
+        Usuario usuario4 = new Usuario(id4, nombre4);
+        assertNull(usuario4.getNombre());
+    }
 
-	    @Test
-	    void CP4() {
-	        // Caso 4: ID válido, nombre nulo.
-	        int id4 = 789;
-	        String nombre4 = null;
-	        Usuario usuario4 = new Usuario(id4, nombre4);
-	        assertNull(usuario4.getNombre());
-	    }
-
-	    @Test
-	    void CP5() {
-	        // Caso 5: ID válido, nombre vacío.
-	        int id5 = 1011;
-	        String nombre5 = "";
-	        Usuario usuario5 = new Usuario(id5, nombre5);
-	        assertEquals(usuario5.getNombre(),"");
-	    }
+    //caja blanca
+    @Test
+    void CP4() {
+        int id = 789;
+        String nombreMalo = "pep.e";
+        Usuario usuario = new Usuario(id, nombreMalo);
+        assertNull(usuario.getNombre());
+    }
 
 }
