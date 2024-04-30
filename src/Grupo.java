240b2b6d83b1dd3b977420f87200e3fac8c27c29
salvapiino <seralpi99@gmail.com>
2024-04-30 12:55:26 +0200

    corregir anhadir usuarios
    correccion del metodo que añade multiples usuarios a un grupo de la clase Grupo, ademas de merge

--- src/Grupo.java
+++ src/Grupo.java
@@ -6,85 +6,82 @@ import java.util.Random;
 
 public class Grupo {
 
-    Integer id;
-    String nombre;
-    HashSet<Usuario> users;
+	Integer id;
+	String nombre;
+	HashSet<Usuario> users;
 
-    public Grupo(){
+	public Grupo() {
 
-    }
-    
-    public Grupo(String nombre, HashSet<Usuario> users) {
-        id = new Random().nextInt();
-        this.nombre = nombre;
-        this.users = new HashSet<>();
-        setUsers(users);
+	}
 
-    }
+	public Grupo(String nombre, HashSet<Usuario> users) {
+		id = new Random().nextInt();
+		this.nombre = nombre;
+		this.users = new HashSet<>();
+		setUsers(users);
 
-    public HashSet<Usuario> getUsers() {
-        return users;
-    }
+	}
 
-    public void setUsers(HashSet<Usuario> users) {
-        if (users != null && !users.isEmpty()) {
-            this.users.addAll(users);
-        }
-    }
+	public HashSet<Usuario> getUsers() {
+		return users;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public void setUsers(HashSet<Usuario> users) {
+		if (users != null && !users.isEmpty()) {
+			this.users.addAll(users);
+		}
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setNombre(String nombre) {
-        if(nombre != null)
-            this.nombre = nombre;
-    }
-    
-    
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if (nombre != null)
+			this.nombre = nombre;
+	}
+
 	public void anadirGasto(Gasto gasto) {
-	
+
 	}
-	
+
 	public void anhadirMultiplesUsuarios(ArrayList<Usuario> usuarios) {
-		if(usuarios != null && !usuarios.isEmpty()) {
-			for(Usuario u : usuarios) {
-				if(u != null && !this.users.contains(u)) {
+		if (usuarios != null) {
+			for (Usuario u : usuarios) {
+				if (u != null && !this.users.contains(u)) {
 					this.users.add(u);
-					//System.out.println("Usuario " + u + " anhadido correctamente al grupo.");
-				}
-				else {
+					// System.out.println("Usuario " + u + " anhadido correctamente al grupo.");
+				} else {
 					// EXCEPCION o RETURN
-					//System.out.println("Error: el usuario " + u + " no existe o ya esta incluido en el grupo.");
+					// System.out.println("Error: el usuario " + u + " no existe o ya esta incluido
+					// en el grupo.");
 				}
 			}
-		}
-		else {
+		} else {
 			// EXCEPCION o RETURN
-			//System.out.println("Error: la lista no existe o está vacía.");
+			// System.out.println("Error: la lista no existe o está vacía.");
 		}
 	}
 
-    public void eliminarMultiplesUsuarios(ArrayList<Usuario> usuarios) {
-		if(usuarios != null && !usuarios.isEmpty()) {
-			for(Usuario u : usuarios) {
-				if(u != null && !this.users.contains(u)) {
+	public void eliminarMultiplesUsuarios(ArrayList<Usuario> usuarios) {
+		if (usuarios != null && !usuarios.isEmpty()) {
+			for (Usuario u : usuarios) {
+				if (u != null && !this.users.contains(u)) {
 					this.users.remove(u);
-					//System.out.println("Usuario " + u + " eliminado correctamente del grupo.");
-				}
-				else {
+					// System.out.println("Usuario " + u + " eliminado correctamente del grupo.");
+				} else {
 					// EXCEPCION o RETURN
-					//System.out.println("Error: el usuario " + u + " no existe o ya no esta incluido en el grupo.");
+					// System.out.println("Error: el usuario " + u + " no existe o ya no esta
+					// incluido en el grupo.");
 				}
 			}
-		}
-		else {
+		} else {
 			// EXCEPCION o RETURN
-			//System.out.println("Error: la lista no existe o está vacía.");
+			// System.out.println("Error: la lista no existe o está vacía.");
 		}
 	}
 }
