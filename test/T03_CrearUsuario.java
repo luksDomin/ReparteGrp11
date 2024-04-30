bcc6ecb8db08db45c060b87103a190fda3ba3dee
ElRuflas <128366266+ElRuflas@users.noreply.github.com>
2024-04-30 10:15:50 +0200

    test CrearUsuario completado
    TG-3 #donecn

--- test/T03_CrearUsuario.java
+++ test/T03_CrearUsuario.java
@@ -4,10 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TO3_CrearUsuario {
+class T03_CrearUsuario {
 
-	
-
+	@Test
 	 void CP1() {
 	        // Caso 1: ID positivo, nombre válido.
 	        int id1 = 123;
@@ -21,18 +20,19 @@ class TO3_CrearUsuario {
 	    void CP2() {
 	        // Caso 2: ID negativo, nombre válido.
 	        int id2 = -456;
-	        String nombre2 = "Usuario2";
+	        String nombre2 = "juan";
 	        Usuario usuario2 = new Usuario(id2, nombre2);
-	        assertNotNull(usuario2);
+	        assertTrue(usuario2.getId() < 0);
+	        assertEquals("juan",usuario2.getNombre());
 	    }
 
 	    @Test
 	    void CP3() {
 	        // Caso 3: ID cero, nombre válido.
 	        int id3 = 0;
-	        String nombre3 = "Usuario3";
+	        String nombre3 = "pepe";
 	        Usuario usuario3 = new Usuario(id3, nombre3);
-	        assertNotNull(usuario3);
+	        assertEquals(usuario3.getId(),0);
 	    }
 
 	    @Test
@@ -41,7 +41,7 @@ class TO3_CrearUsuario {
 	        int id4 = 789;
 	        String nombre4 = null;
 	        Usuario usuario4 = new Usuario(id4, nombre4);
-	        assertNull(usuario4);
+	        assertNull(usuario4.getNombre());
 	    }
 
 	    @Test
@@ -50,7 +50,7 @@ class TO3_CrearUsuario {
 	        int id5 = 1011;
 	        String nombre5 = "";
 	        Usuario usuario5 = new Usuario(id5, nombre5);
-	        assertNull(usuario5);
+	        assertEquals(usuario5.getNombre(),"");
 	    }
 
 }
