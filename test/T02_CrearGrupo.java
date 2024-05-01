df739af8230028dde9ce2f65225e9db13d574838
Paula Vázquez <115453280+paaulavazz@users.noreply.github.com>
2024-05-01 11:57:10 +0200

    TG-2 #donecb
    Acabadas las pruebas de caja blanca de CrearGrupo

--- test/T02_CrearGrupo.java
+++ test/T02_CrearGrupo.java
@@ -27,9 +27,35 @@ class T02_CrearGrupo {
 		brais = null;
 		david = null;
 	}
+	
 
+	/*Pruebas de caja negra*/
+	
+	/*CP1: Nombre del grupo nulo.*/
 	@Test
-	void testCrearGrupoCP1() {
+	void testCrearGrupoCP1(){
+		//Act
+		Grupo grupoPrueba = new Grupo(null, u);
+
+		//Assert
+		assertNull(grupoPrueba.getNombre()); // Comprobamos que el nombre del grupo es nulo
+		
+	}
+	
+	/*CP3: HashSet de usuarios nulo.*/
+	@Test
+	void testCrearGrupoCP3(){
+		//Act
+		Grupo grupoPrueba = new Grupo("nombreGrupo", null);
+
+		//Assert
+		assertNull(grupoPrueba.getUsers()); // Comprobamos que el HashSet de usuarios es nulo
+		
+	}
+	
+	/*CP5: Grupo creado correctamente.*/
+	@Test
+	void testCrearGrupoCP5() {
 		
 		//Act
 		Grupo grupoPrueba = new Grupo("grupoPrueba", u);
@@ -41,37 +67,32 @@ class T02_CrearGrupo {
 		assertSame("grupoPrueba", grupoPrueba.getNombre(), "El constructor ha fallado estableciendo el nombre");
 		assertEquals(u , grupoPrueba.getUsers(), "El constructor ha fallado estableciendo los usuarios");
 	}
-
+	
+	
+	
+	/*Pruebas de caja blanca*/
+	
+	/*CP2: Nombre del grupo contiene caracteres que no son letras o números.*/
 	@Test
-	void testCrearGrupoCP2(){
-		//Act
-		Grupo grupoPrueba = new Grupo(null, u);
-
-		//Assert
-		assertNull(grupoPrueba.nombre); // Comprobamos que el nombre del grupo es nulo
-		assertNotNull(grupoPrueba.users); // Comprobamos que el HashSet de usuarios no es nulo
+	void testCrearGrupoCP2() {
 		
+		//Act
+		Grupo grupoPrueba = new Grupo("gru-po", u);
+		
+		//Assert
+		assertNull(grupoPrueba.getNombre()); // Comprobamos que el grupo no se ha creado.
 	}
-
+	
+	/*CP4: HashSet de usuarios vacío*/
 	@Test
-	void testCrearGrupoCP3(){
-		//Act
-		Grupo grupoPrueba = new Grupo("grupoPrueba", null);
-
-		//Assert
-		assertNotNull(grupoPrueba.nombre); // Comprobamos que el nombre del grupo no es nulo
-		assertNull(grupoPrueba.users); // Comprobamos que el HashSet de usuarios es nulo
+	void testCrearGrupoCP4() {
 		
-	}
-
-	@Test
-	void testCrearGrupoCP4(){
-		//Act
-		Grupo grupoPrueba = new Grupo(null, null);
-
-		//Assert
-		assertNull(grupoPrueba.nombre); // Comprobamos que el nombre del grupo es nulo
-		assertNull(grupoPrueba.users); // Comprobamos que el HashSet de usuarios es nulo
+		HashSet<Usuario> usuariosVacio = new HashSet<Usuario>();
 		
+		//Act
+		Grupo grupoPrueba = new Grupo("grupoPrueba", usuariosVacio);
+		
+		//Assert
+		assertNull(grupoPrueba.getUsers()); // Comprobamos que el grupo no se ha creado.
 	}
 }
